f774077952e6110a1f5470c443197094c998a1eb
learn-github190 <asg1996@live.com>
2023-04-20 21:17:34 +0300

    remove unused import statements

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan({ "proxies", "repositories", "services" })
 @ComponentScan({ "services", "proxies", "repositories" })
 public class Config {
 }
